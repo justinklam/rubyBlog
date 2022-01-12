f9214e6bccc65c51d993762162bede63d0325aa4
Justin Lam <justin.kh.lam@gmail.com>
2022-01-12 02:52:26 +0000

    add new.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
+# Controller - class that is defined to inherit from ApplicationController
 class ArticlesController < ApplicationController
+  # We define methods within here to become the actions of the controller
+  def new
+  end
 end
