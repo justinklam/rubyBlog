696f884920664fcf645aef03522fcfd85f08a7b7
Justin Lam <justin.kh.lam@gmail.com>
2022-01-12 05:47:55 +0000

    add additional logic to comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,14 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     # @comment auto links association through .create
     @comment = @article.comments.create(comment_params)
-
     # calls the show view
+    redirect_to article_path(@article)
+  end
+ 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
  
