a0ccb0218c4d444553d69399a94c282195677838
Justin Lam <justin.kh.lam@gmail.com>
2022-01-12 03:19:39 +0000

    add in def create in articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 # Controller - class that is defined to inherit from ApplicationController
 class ArticlesController < ApplicationController
   # We define methods within here to become the actions of the controller
+  
   def new
   end
+
+  def create
+    # render taking in hash of plain and value of params[:article].inspect
+    # params method is the object which represents the parameters(or fields) coming in from the form ie. actual username/email
+    render plain: params[:article].inspect
+  end
+
 end
