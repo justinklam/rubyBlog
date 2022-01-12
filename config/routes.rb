6136b79b50d1d4c48b6a630a7adca448dc7d981e
Justin Lam <justin.kh.lam@gmail.com>
2022-01-12 05:24:24 +0000

    ran rails generate controller Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  resources :articles
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  get 'welcome/index'
+
+  resources :articles do
+    resources :comments
+  end
+  
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
